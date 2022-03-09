839f5bf1d6155f14c02da1c625513ef51e40962b
Anushree Dutt <anushreedutt@gmail.com>
2022-03-09 13:29:48 +0530

    login.js modified

--- login.js
+++ login.js
@@ -1,4 +1,15 @@
 
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
