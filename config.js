218915caeb3879580d4feaabcd1e5e696c60255b
JadenFurtado <52862591+JadenFurtado@users.noreply.github.com>
2022-03-09 11:40:34 +0530

    changed firebased config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBLNaBZaK1C7gjZswpYEhnwK3dXPRblzm0",
-    authDomain: "swaasthya-eb85c.firebaseapp.com",
-    projectId: "swaasthya-eb85c",
-     databaseURL: "https://swaasthya-eb85c-default-rtdb.asia-southeast1.firebasedatabase.app",
-    storageBucket: "swaasthya-eb85c.appspot.com",
-    messagingSenderId: "584617120815",
-    appId: "1:584617120815:web:c303fa46b7229e3421e2da",
-    measurementId: "G-MHHNLM5KXP"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCSgfMVfFgvMzMwBUdOrHGIJpSZ2cwQqwU",
+  authDomain: "screenplay-4167f.firebaseapp.com",
+  databaseURL: "https://screenplay-4167f.firebaseio.com",
+  projectId: "screenplay-4167f",
+  storageBucket: "screenplay-4167f.appspot.com",
+  messagingSenderId: "393855359637",
+  appId: "1:393855359637:web:ac46bbac3bb7b74cb6999c",
+  measurementId: "G-NW7WKFTKKE"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
